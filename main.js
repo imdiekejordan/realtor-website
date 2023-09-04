9f92bff9858723218293ea968e663833ed270fa4
imdiekejordan <96355039+imdiekejordan@users.noreply.github.com>
2023-09-03 19:23:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+// Initialize Swiper JS
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+
+// Hamburger Menu
+const hamburger = document.querySelector(".hamburger");
+const border = document.querySelector(".border");
+const contactimage = document.querySelector(".contact-image");
+const contactBubble = document.querySelector(".contact-bubble");
+const navlink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    border.classList.toggle("active");
+    contactimage.classList.toggle("active");
+    contactBubble.classList.toggle("active");
+}
+
+for (var i = 0; i < navlink.length; i++) {
+    navlink[i].addEventListener('click', mobileMenu, false);
+}
+
+
+
+
+
+// FAQ
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
